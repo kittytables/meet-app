bc23d7eab5784c56a2f747d8b0578a0c3903a859
kittytables <tran.kathleen91@gmail.com>
2016-10-12 21:40:22 -0400

    add validation to inputs in signup form

--- src/Signup.js
+++ src/Signup.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 import './Signup.css';
 
-var errors = {
-    name: 'Please input a name',
-    email: 'Please enter a valid email address',
-    password: 'Include at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character'
-}
-
 export default class Signup extends Component {
     constructor(props) {
         super(props);
@@ -26,13 +20,26 @@ export default class Signup extends Component {
     }
 
     submit = () => {
-        console.log('hello', this.state)
+        console.log('hello', this.state);
     }
 
-    validateName = (event, errorKey) => {
+    validateInput = (event, errorKey) => {
         const isValid = event.target.checkValidity();
         this.setState({
             [errorKey]: !isValid
+        });
+
+        if (errorKey == 'hasConfirmPasswordError' &&
+                this.state.password != this.state.confirmPassword) {
+            this.setState({
+                [errorKey]: true
+            });
+        }
+    }
+
+    reset = (event, errorKey) => {
+        this.setState({
+            [errorKey]: null
         });
     }
 
@@ -49,7 +56,7 @@ export default class Signup extends Component {
                         autoFocus="true"
                         autoComplete="name"
                         placeholder="e.g. Jane Doe"
-                        onBlur={(event) => this.validateName(event, 'hasNameError')}
+                        onBlur={(event) => this.validateInput(event, 'hasNameError')}
                         required
                     />
                     {this.state.hasNameError && (
@@ -60,37 +67,55 @@ export default class Signup extends Component {
                 <label htmlFor="email">
                     <span>Email</span>
                     <input
-                       value={this.state.email}
-                       id="email"
-                       type="text"
-                       autoComplete="email"
-                       placeholder="name@email.com"
-                       required
-                       pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" onBlur={this.validateInput}
+                        className={this.state.hasEmailError ? 'invalid' : ''}
+                        value={this.state.email}
+                        onChange={(event) => this.setState({email: event.target.value})}
+                        id="email"
+                        type="text"
+                        autoComplete="email"
+                        placeholder="name@email.com"
+                        required
+                        pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+                        onBlur={(event) => this.validateInput(event, 'hasEmailError')}
                    />
+                   {this.state.hasEmailError && (
+                        <span className="error-message">Please enter a valid email address</span>
+                    )}
                </label>
 
                 <label htmlFor="password">
                     <span>Password</span>
                     <input
+                        className={this.state.hasPasswordError ? 'invalid' : ''}
                         value={this.state.password}
+                        onChange={(event) => this.setState({password: event.target.value})}
                         id="password"
                         type="password"
                         placeholder="****************"
-                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}"
                         required
+                        pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                        onBlur={(event) => this.validateInput(event, 'hasPasswordError')}
                     />
+                    {this.state.hasPasswordError && (
+                        <span className="error-message">Please enter an 8-character password with at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character</span>
+                    )}
                 </label>
 
                 <label htmlFor="confirm-password">
                     <span>Confirm Password</span>
                     <input
+                        className={this.state.hasConfirmPasswordError ? 'invalid' : ''}
                         value={this.state.confirmPassword}
+                        onChange={(event) => this.setState({confirmPassword: event.target.value})}
                         id="confirm-password"
                         type="password"
-                        placeholder="****************"
                         required
+                        placeholder="****************"
+                        onBlur={(event) => this.validateInput(event, 'hasConfirmPasswordError')}
                     />
+                    {this.state.hasConfirmPasswordError && (
+                        <span className="error-message">Please enter the same password value as above.</span>
+                    )}
                 </label>
 
                 <button
